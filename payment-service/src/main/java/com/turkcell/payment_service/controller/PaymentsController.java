b0098995208cae09d150b3a051ad1fa9984045b5
nursultanaslan <nursultanaslan1@gmail.com>
2025-10-13 17:15:54 +0300

    initial commit

---
+++ payment-service/src/main/java/com/turkcell/payment_service/controller/PaymentsController.java
@@ -0,0 +1,15 @@
+package com.turkcell.payment_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/payments")
+public class PaymentsController {
+
+    @GetMapping
+    public String get(){
+        return "Welcome Payment-Service";
+    }
+}
